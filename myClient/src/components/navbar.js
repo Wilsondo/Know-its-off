270f76506d6ca85b7e81b1e959375c267014b85c
Tonyenike <tonyenike@gmail.com>
2020-02-11 15:58:30 -0800

    Added logout functionality, profile still TBD

--- myClient/src/components/navbar.js
+++ myClient/src/components/navbar.js
@@ -6,16 +6,16 @@ export default class MyNavbar extends Component {
 		return(
 
 <nav className="navbar navbar-expand-lg navbar-light bg-info">
-  <a className="navbar-brand" href="#">Navbar</a>
+  <Link to="/appliances" className="navbar-brand">Know It's Off</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div className="navbar-nav">
-      <Link to="/" className="nav-item nav-link active">Home </Link>
+      <Link to="/appliances" className="nav-item nav-link active">Home </Link>
       <Link to="/appliances" className="nav-item nav-link">My appliances</Link>
-      <Link to="/user" className="nav-item nav-link">My profile</Link>
-      <Link to="/logout" className="nav-item nav-link disabled">Logout</Link>
+      <Link to="/profile" className="nav-item nav-link">My profile</Link>
+      <Link to="/" className="nav-item nav-link">Logout</Link>
     </div>
   </div>
 </nav>

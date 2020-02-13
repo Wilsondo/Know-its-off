afcbd569523d242c93a60c94f667fb8512d76ee2
Tonyenike <tonyenike@gmail.com>
2020-02-13 14:01:00 -0800

    Add build folder, so you don't have to build client side yourself

---
+++ myClient/build/precache-manifest.5a6e97340104185d79ae2210bdf5e166.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8089e4152f9010694a76d5eea8c7b3e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05aa329d53495211b28c",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6cde8cb80244a31d0119",
+    "url": "/static/css/main.dc752c7d.chunk.css"
+  },
+  {
+    "revision": "05aa329d53495211b28c",
+    "url": "/static/js/2.398ec610.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.398ec610.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6cde8cb80244a31d0119",
+    "url": "/static/js/main.5e0fa036.chunk.js"
+  },
+  {
+    "revision": "2c31c5642ff2e090eee0",
+    "url": "/static/js/runtime-main.bc77f7ab.js"
+  }
+]);

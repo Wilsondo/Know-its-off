ceaa4f9d75e229573657b4d2f45c958139c845f8
Tonyenike <tonyenike@gmail.com>
2020-02-13 16:11:46 -0800

    Added mor stuff

---
+++ myClient/build/precache-manifest.5b578caeda929f6996696fe1d77a5d64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c64a735bde9a87b6a8e950349cd102b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05aa329d53495211b28c",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "36a50b837dcfbb01714e",
+    "url": "/static/css/main.dc752c7d.chunk.css"
+  },
+  {
+    "revision": "05aa329d53495211b28c",
+    "url": "/static/js/2.398ec610.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.398ec610.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36a50b837dcfbb01714e",
+    "url": "/static/js/main.3e9c8fd3.chunk.js"
+  },
+  {
+    "revision": "2c31c5642ff2e090eee0",
+    "url": "/static/js/runtime-main.bc77f7ab.js"
+  }
+]);
--- myClient/build/service-worker.js
+++ myClient/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/~martinb3/know-its-off/precache-manifest.1760ebf512dfb3dd215ddcf46f5ac0ec.js"
+  "/precache-manifest.5b578caeda929f6996696fe1d77a5d64.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/~martinb3/know-its-off/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

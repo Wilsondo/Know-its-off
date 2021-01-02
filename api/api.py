7b390b2bdc3463efbe9b17daae034bcf00c76f73
titswort <titswort@oregonstate.edu>
2021-01-01 19:17:59 -0800

    bug fixes

--- api/api.py
+++ api/api.py
@@ -1,16 +1,14 @@
 import time
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-<<<<<<< HEAD
-=======
 app.config.from_object(Config)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-import models
->>>>>>> 7d9f05b... first commit
 
 @app.route('/')
 def hello(): 

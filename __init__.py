2759d092f6e2ce9c3a743c25c6e91020d3538aea
titswort <titswort@oregonstate.edu>
2021-01-25 17:49:49 -0800

    consolidating master for production

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# This file is just to declare this whole directory as a package. 
-# 
-# This allows the routes (in the routes folder) to import and 
-# share the db that is created in main.py
-
-from main import app

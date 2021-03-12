0426747b102545757975ff89e6e304ea1a5734e8
Wilsondo <wilsondo@oregonstate.edu>
2021-03-11 18:06:24 -0800

    Merged some stuff

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 const axiosBaseURL = Axios.create({
     // 3000 for react-scripts (yarn start-dev)
     // 5000 for /build (yarn start)
-    //baseURL:'https://624c7fc5b9d1.ngrok.io/api/',
-    baseURL:'http://localhost:5000/api/',
+    baseURL:'https://8cb83084acc2.ngrok.io/api',
+    //baseURL:'http://localhost:3000/api/',
 });
 export default axiosBaseURL

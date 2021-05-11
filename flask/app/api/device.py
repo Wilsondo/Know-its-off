75c520e4cefadfcd27632d19bab7a4a6af01e74c
titswort <titswort@oregonstate.edu>
2021-05-11 09:24:05 -0700

    fix AWS bugs

--- flask/app/api/device.py
+++ flask/app/api/device.py
@@ -1,3 +1,12 @@
+#****************************************************************************************************
+# FILENAME: device.py
+# DESCRIPTION: This file contains routes that deal with the device.
+# AUTHOR(S): Capstone 2020-2021 Douglas Wilson
+# NOTES: Remember that every route has an /api written before it.
+# /device/id becomes /api/device/id
+# If you want to learn about SQLalchemy sessions look here
+# https://www.pythoncentral.io/understanding-python-sqlalchemy-session/
+#****************************************************************************************************
 from flask import request, abort, jsonify
 from flask_login import login_required, current_user
 from app.api import bp
@@ -6,34 +15,40 @@ from app import db
 from cerberus import Validator
 from flask import abort
 
-#Note token_auth is disabled as the backend never seems to recieve the token
-#This may be because of our odd problems working with local host
-#Change login_required to token_auth.login_required when we get a server
 
-
+#The device shema in combination with the Validator user Ceberus to ensure that the incoming requests
+#dealing with the device are good. (I.e. if they are missing a field it will freak out.)
 device_schema = {
                     "appliance_name": {"type": "string", "maxlength": 64, "nullable": True}, 
                     "device_state": {"type": "integer", "default": False, "nullable": False},
-                    "device_battery": {"type": "float", "nullable": True}, # float change position reminder
+                    "device_battery": {"type": "float", "nullable": True},
                     "timestamp": {"type": "datetime", "nullable": True}
 }
 
 v = Validator(device_schema, allow_unknown=True)
 
-#Multi use Route to get specific information about devices
+#Multi use Route to get and modify specific information about devices 
 @bp.route('/device/<id>', methods=['GET', 'PATCH', 'DELETE'])
+#The login_required route requires the user to be logged in otherwise
+#401 UNAUTHORIZED will be returned.
 @login_required
 def device_get_patch_delete_by_id(id):
     #SELECT *
     #FROM device
     #WHERE device.id = id AND device.user_id = current_user id
+    #LIMIT 1
     #We make sure that the current_user can't put down devices 
     #that they do not have access to
     myDevice = Device.query.filter_by(id=id, user_id=current_user.get_id()).first()
     
     #Returns the specific device
     if request.method == 'GET':
+        #Values need to be in JSON to be sent back correctly
+        #But they need to be turned into a dictionary before
+        #they can be turned into JSON
         returnValue = jsonify(myDevice.to_dict())
+        #db.session.close gives the thread back to the connection pool, 
+        #read the tutorial back in the notes
         db.session.close
         return returnValue, 200
     #Updates the device tuple with new information
@@ -41,6 +56,7 @@ def device_get_patch_delete_by_id(id):
         if not v.validate(request.get_json()):
             abort(400, description=v.errors)
         myDevice.update(request.get_json())
+        #The changes are not saved to the database until you commit them.
         db.session.commit()
         returnValue = jsonify(myDevice.to_dict())
         db.session.close()
@@ -54,6 +70,7 @@ def device_get_patch_delete_by_id(id):
         print(myDevice, " Removed")
         return '', 204
 
+#Gets every single device in the database, used in the edit device page.
 @bp.route('/allDevices', methods=['GET'])
 @login_required
 def getAllDevices():
@@ -68,6 +85,9 @@ def getAllDevices():
 @login_required
 def getDeviceLogs(id):
     if request.method == 'GET':
+        #SELECT * 
+        #FROM BatteryLogger
+        #WHERE device_id = id
         myLogs = BatteryLogger.query.filter_by(device_id = id).all()
         returnValue = []
         for row in myLogs:
@@ -76,32 +96,34 @@ def getDeviceLogs(id):
             del row["id"]
         return jsonify(returnValue), 200
 
+#This route returns all of the devices that belong to the current user
+#It can get the current users id from flask
+#It also does some work on the timestamp to make it prettier
+#Be warned that the timestamp is based upon the servers timezone.
+#This must be handled on the front end.
 @bp.route('/devices', methods=['GET'])
 @login_required
 def getUserDevices():
+    #SELECT * 
+    #FROM BatteryLogger
+    #WHERE device_id = id
     results = Device.query.filter_by(user_id = current_user.get_id())
     myList = []
     for row in results:
-        #TODO check if there's a way to convert time to local (currently set to server locale only)
-        #Format M/D/Y HR:MIN AM/PM\
         rowDict = row.to_dict() 
-        if rowDict['timestamp'] != None:
-            given_date = rowDict['timestamp']
-            given_date = given_date.strftime("%A %-I:%M %p, %B %d %Y")
-            rowDict['timestamp'] = given_date
-            myList.append(rowDict)
-        else:
-            rowDict['timestamp'] = "N/A"
-            myList.append(rowDict)
+        myList.append(rowDict)
 
     db.session.close()
 
     return jsonify(myList), 200
 
-@bp.route('/device', methods=['POST', 'GET'])
+#Gets information about a new device from the front end
+#and saves it to the database.
+@bp.route('/device', methods=['POST'])
 @login_required
 def device_get_post():
     if request.method == 'POST':
+        #Validates the device, if it doesn't match the schema it will be thrown out.
         if not v.validate(request.get_json()):
             abort(400, description=v.errors)
         new_device = Device(**request.get_json())
@@ -113,12 +135,3 @@ def device_get_post():
         db.session.close()
         print(new_device, " Added")
         return returnValue, 201
-    elif request.method == 'GET':
-        #Select *
-        #From Device
-        results = Device.query
-        myList = []
-        for row in results:
-            myList.append(row.to_dict())
-        db.session.close()
-        return jsonify(myList), 200
--- flask/app/api/handler.py
+++ flask/app/api/handler.py
@@ -1,3 +1,11 @@
+#****************************************************************************************************
+# FILENAME: handler.py
+# DESCRIPTION: The handler.py file contains the route that controls interaction between the
+# physical device and the database.
+# AUTHOR(S): Capstone 2020-2021 Douglas Wilson
+# NOTES: Remember that every route has an /api written before it.
+# /device/id becomes /api/device/id
+#****************************************************************************************************
 import os
 from flask import Flask, request
 from config import Config
@@ -8,11 +16,14 @@ from app import db
 import json
 from app.models import Device, BatteryLogger
 
-#These variables define the battery voltages
+#These variables define the battery values coming in.
 #We can use these to calculate the battery power level
 #as a percentage.
-Vmax = 4.2
-Vmin = 3.0
+#This used to be voltage
+Vmax = 1023
+Vmin = 0
+
+#Sending values between 0 - 1023
 
 #We want to get the device iD, then modify the corresponding device
 @bp.route('/updateState/<int:device_id>', methods=['PATCH'])
@@ -20,11 +31,10 @@ def device_data_post(device_id):
    if request.method == "PATCH":
       print("Receiving Device at ID: ", device_id)
       deviceStats = request.get_json()
-      deviceStats = json.loads(deviceStats)
       #Set the time the state of the device changed to right now
       deviceStats['timestamp'] = datetime.now()      
 
-      #Convert voltage to battery power.d
+      #Convert voltage to battery power.
       deviceVoltage = deviceStats['device_battery']
       # (Vactual - Vmin) * 100
       # ______________________
@@ -39,13 +49,16 @@ def device_data_post(device_id):
       #Where id = device_id
       #LIMIT 1
       myDevice = Device.query.filter_by(id=device_id).first()
-      myDevice.update(deviceStats)
+      if myDevice:
+         myDevice.update(deviceStats)
 
-      myStamp = BatteryLogger()
-      myStamp.device_battery = deviceStats['device_battery']
-      myStamp.timestamp_time = deviceStats['timestamp']
+         myStamp = BatteryLogger()
+         myStamp.device_battery = deviceStats['device_battery']
+         myStamp.timestamp_time = deviceStats['timestamp']
 
-      #This adds a battery log associated with the device's ID
-      myDevice.battery_logger.append(myStamp)
-      db.session.commit()
-      return '', 200
+         #This adds a battery log associated with the device's ID
+         myDevice.battery_logger.append(myStamp)
+         db.session.commit()
+         return '', 204
+      else: 
+         return '', 403

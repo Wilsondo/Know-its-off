72a9abbdaadecabb69489378956e4465ceb88c39
Skyhook <35271544+Wilsondo@users.noreply.github.com>
2021-05-26 15:47:46 -0700

    Create example-constants.py

---
+++ example-constants.py
@@ -0,0 +1,7 @@
+# These are example constants for the database.
+# Fill these in and change the filename to constants.py
+
+username="myUsername"
+password="myPassword"
+database="myDatabase"
+host="myHost"

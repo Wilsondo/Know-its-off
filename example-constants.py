e1d5aac0a3e6e874c1c7895a5613b9b8fbd42db4
Skyhook <35271544+Wilsondo@users.noreply.github.com>
2021-05-26 15:50:53 -0700

    Update example-constants.py

--- example-constants.py
+++ example-constants.py
@@ -1,5 +1,6 @@
 # These are example constants for the database.
 # Fill these in and change the filename to constants.py
+# Never upload this file to Github to protect sensitive information.
 
 username="myUsername"
 password="myPassword"

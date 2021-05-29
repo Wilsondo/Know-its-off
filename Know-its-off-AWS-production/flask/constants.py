9ce1cf2b23e5084d4ab0385009dbd85a4e44f0e8
Skyhook <35271544+Wilsondo@users.noreply.github.com>
2021-05-28 23:05:30 -0700

    Update constants.py

--- Know-its-off-AWS-production/flask/constants.py
+++ Know-its-off-AWS-production/flask/constants.py
@@ -1,5 +1,5 @@
-username="admin"
-password="Ijy9*7,YrR)7b#kytQu5"
-database="aws_capstone_know_its_off"
-host="aws-capstone-know-its-off-1.ctrcixjmgcxc.us-east-2.rds.amazonaws.com"
+username="<DBusername>"
+password="<DBpassword>"
+database="<DBname>"
+host="<AWShost>"
 
